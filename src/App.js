f0d0506e159a39465118b43d1e001858ce497247
Mujtaba <mujtabaali650@gmail.com>
2022-10-25 17:59:40 +0500

    CV

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Accordian from './component/Accordian';
+import ProfileCv from './component/ProfileCv';
+import Progress from './component/Progress';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='row'>
+        <div className='col-md-3'><ProfileCv/></div>
+        <div className='col-md-9'><Accordian/>
+        <div className='col-md-9'><Progress/></div></div>
+        
+      </div>
     </div>
   );
 }
